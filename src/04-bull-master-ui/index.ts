04597dfedb06f6a2b636f3ca8c2895e5978b7190
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-11-20 23:38:30 +0200

    refactor example 04

--- src/04-bull-master-ui/index.ts
+++ src/04-bull-master-ui/index.ts
@@ -1,25 +1,10 @@
-// https://github.com/hans-lizihan/bull-master
-
-import express from "express";
 import {Job} from "bullmq";
-import open from "open";
 import {createQueue, createWorker, Message, randomMessage} from "../commons";
-
-const bullMaster = require("bull-master");
-
 
 const INTERVAL = 0.2 * 1000;
 const QUEUE_NAME = "queue-04"
 
 const queue = createQueue(QUEUE_NAME)
-const app = express()
-
-
-const bullMasterApp = bullMaster({
-    queues: [queue]
-});
-
-app.use('/', bullMasterApp)
 
 const worker = createWorker(QUEUE_NAME, async (job: Job<Message>) => {
     if (Math.random() < 0.5)
@@ -34,6 +19,3 @@ setInterval(async () => {
     await queue.add(name, message)
 }, INTERVAL);
 
-app.listen(8080, async () => {
-    await open('http://localhost:8080', {wait: true});
-});
--- src/commons/index.ts
+++ src/commons/index.ts
@@ -1,13 +1,20 @@
 import {Processor, Queue, QueueOptions, Worker,} from 'bullmq';
 import {WorkerOptions} from "bullmq/src/interfaces/index";
 import * as faker from "faker";
+import express from "express";
+
+const bullMaster = require("bull-master");
 
 export type Message = {
     name: string,
     createdAt: Date
 }
 
-export const createQueue = (queueName: string, opts?: QueueOptions) => new Queue<Message>(queueName, opts);
+export const createQueue = (queueName: string, opts?: QueueOptions) => {
+    const queue = new Queue<Message>(queueName, opts)
+    attachUI(queue);
+    return queue;
+};
 
 export const createWorker = (queueName: string, processor: Processor, opts?: WorkerOptions) =>
     new Worker<Message>(queueName, processor, opts);
@@ -16,3 +23,17 @@ export const randomMessage = (): Message => ({
     name: faker.internet.userName(),
     createdAt: new Date()
 })
+
+export const attachUI = (queue: Queue) => {
+    const app = express()
+
+    const bullMasterApp = bullMaster({
+        queues: [queue]
+    });
+
+    app.use('/', bullMasterApp)
+    app.listen(8080, async () => {
+        console.log(`open http://localhost:8080`);
+        // await open('http://localhost:8080', {wait: true});
+    });
+}
